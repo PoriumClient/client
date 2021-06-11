599643d3fa6d8016f81bf7d350e211cd0e7dbc5a
expando <70826967+Cxiro@users.noreply.github.com>
2021-06-11 15:18:39 -0400

    1.17 support

--- Porium/Porium/Sauce/Module/Mods/Graphics/Fullbright.cpp
+++ Porium/Porium/Sauce/Module/Mods/Graphics/Fullbright.cpp
@@ -5,13 +5,13 @@ Fullbright::Fullbright() : Module::Module("Fullbright", "Graphics", 0x46) {
 }
 
 void Fullbright::onEnable() {
-	auto d = (float*)Porium::GetPointerAddress(Porium::moduleBase + 0x03948848, { 0x28, 0x148, 0x18 });
+	auto d = (float*)Porium::GetPointerAddress(Porium::moduleBase + 0x03F57C28, { 0x18, 0x150, 0x18 });
 	*d = 1000;
 }
 
 void Fullbright::onLoop() {}
 
 void Fullbright::onDisable() {
-	auto d = (float*)Porium::GetPointerAddress(Porium::moduleBase + 0x03948848, { 0x28, 0x148, 0x18 });
+	auto d = (float*)Porium::GetPointerAddress(Porium::moduleBase + 0x03F57C28, { 0x18, 0x150, 0x18 });
 	*d = 1;
 }
